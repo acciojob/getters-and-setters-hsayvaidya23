7ef9ac28e7c96378c863e7d97a9ca7eda86a2b41
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-08-29 00:21:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 //complete this code
 class Person {
 	constructor(name, age) {
-		this.name = name;
-		this.age = age;
-	}
-
-	get name() {
-		return this.name
-	}
-	get age() {
-		return this.age;
-	}
-	set age(newage) {
-		this.age = newage;
-	} 
+        this._name = name;
+        this._age = age;
+    }
+    
+    get name() {
+        return this._name;
+    }
+    
+    set name(newName) {
+        this._name = newName;
+    }
+    
+    get age() {
+        return this._age;
+    }
+    
+    set age(newAge) {
+        this._age = newAge;
+    } 
 }
 
 class Student extends Person {
 	constructor(name, age) {
         super(name, age);
     }
-	study() {
-		console.log(`${this.name} is studying`);
-	}
+    
+    study() {
+        console.log(`${this.name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-	constructor(name, age) {
+	 constructor(name, age) {
         super(name, age);
     }
-	teach() {
-		console.log(`${this.name} is teaching`);
-	}
+    
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
 }
 
 
